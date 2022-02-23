a5ce3c7b5fec6b7aa61249b78e2fa01d65acfde6
Fernando Nicolas <fersanti2896@gmail.com>
2022-02-23 16:11:25 -0600

    Realizando petición al API de Gifs mediante el módulo http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+  private apiKey = 'fgqXZEJAho3ZnAxqJm2yftTQuFKhtkUH';
   private _historial: string[] = [];
 
   get historial() {
     /* Rompemos la referencia al arreglo original usando el operador spread */
     return [...this._historial];
   }
+
+  /* Inyectamos el mpodule */
+  constructor(private http: HttpClient) { }
 
   buscarGifs(query: string = '') {
     query = query.trim().toLocaleLowerCase();
@@ -23,5 +28,10 @@ export class GifsService {
     }
 
     console.log(this._historial);
+
+    /* Realizando la petición HTTP con el módulo httpClient al API de Gifs */
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=fgqXZEJAho3ZnAxqJm2yftTQuFKhtkUH&q=dr house&limit=10').subscribe((resp: any) => {
+      console.log(resp.data)
+    });
   }
 }
